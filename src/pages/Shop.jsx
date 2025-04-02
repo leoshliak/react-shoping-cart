8d2baa4cedb52c49908db74c74724bc3f9d00ff6
leoshliak <levshlyak@gmail.com>
2025-04-02 15:01:53 +0300

    search functional

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -9,7 +9,11 @@ const Shop = () => {
   const [currentCategory, setCurrentCategory] = useState('all');
   const [hoveredProductId, setHoveredProductId] = useState(null);
   const [loadedHoverImages, setLoadedHoverImages] = useState({});
+  const [search, setSearch] = useState('')
   const navigate = useNavigate();
+  const searchResults = products.filter(product => 
+    product.title.toLowerCase().includes(search.trim().toLowerCase())
+  );
 
   useEffect(() => {
     if (products.length > 0) {
@@ -56,10 +60,13 @@ const Shop = () => {
           </div>
         </aside>
         <main>
-          <div className="top"><h2>{categories.find(c => c.id === currentCategory)?.label || 'All products'}</h2></div>
+          <div className="top"><h2>{categories.find(c => c.id === currentCategory)?.label || 'All products'}
+            </h2>{currentCategory ==='all' ? <div className="search"><i class="fa-solid fa-magnifying-glass"></i><input type="search" onChange={(e) => {setSearch(e.target.value)
+              console.log(searchResults)
+            }} value={search} /></div> : ''}</div>
           <div className="products">
-            {currentCategory === 'all' ? 
-            products.map((product) => (
+            {currentCategory === 'all' && searchResults === '' ? 
+            (products.map((product) => (
               <div className='product-card' key={product.id}
               onMouseEnter={() => setHoveredProductId(product.id)}
               onMouseLeave={() => setHoveredProductId(null)}
@@ -109,7 +116,57 @@ const Shop = () => {
       console.log(cart)
       }}>Add to cart</button></div>
     </div>
-            ))
+            ))) : currentCategory === "all" && currentCategory !== '' ? (searchResults.map((product) => (
+              <div className='product-card' key={product.id}
+              onMouseEnter={() => setHoveredProductId(product.id)}
+              onMouseLeave={() => setHoveredProductId(null)}
+              onClick={(e) => {
+                if (!e.target.closest('button')) {
+                  navigate(`/shop/${product.id}`, { 
+                    state: { product}
+                  });
+                  window.scrollTo({
+                    top: 0,
+                    behavior: 'auto'
+                  });
+                }
+              }}
+              >
+      <div className="image-wrapper">
+       <img src={product.thumbnail} alt={product.title} className="main-image"
+         style={{ 
+           opacity: hoveredProductId === product.id ? 0 : 1,
+           transition: 'opacity 0.4s ease-in-out' 
+               }}
+  />
+       <img src={product.images[1]}  alt={product.title} className="hover-image"
+          style={{ 
+          opacity: hoveredProductId === product.id && loadedHoverImages[product.id] ? 1 : 0,
+          transition: 'opacity 0.4s ease-in-out' 
+                }}
+  />
+   </div>
+      <div className="rest">
+      <h3>{product.title}</h3>
+      <p className='price'>{product.price}$</p>
+      <button onClick={() => {
+        const check = cart.find(item => item.title === product.title);
+        setProductsQuantity(prevQuantity => prevQuantity + 1)
+        if(check == undefined) {
+        setCart(prevCart => [...prevCart, {...product, quantity: 1}]);
+      } else {
+        const existingIndex = cart.findIndex(item => item.title === product.title);
+        setCart(prevCart => 
+          prevCart.map((item, index) => ( 
+            index === existingIndex
+              ? { ...item, quantity: item.quantity + 1 }
+              : item))
+        );
+      }
+      console.log(cart)
+      }}>Add to cart</button></div>
+    </div>
+            ))) 
             : products.filter((product) => (product.category === currentCategory)).map((product) =>(
               <div className='product-card' key={product.id}
               onMouseEnter={() => setHoveredProductId(product.id)}
@@ -157,6 +214,9 @@ const Shop = () => {
               }}>Add to cart</button></div>
             </div>
             ))}
+            {searchResults.length === 0 && search.length > 0 && (
+           <div className="no-results">No products found for "{search}"</div>
+           )} 
           </div>
         </main>
     </div>
